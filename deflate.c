3e5eee3e17eafe41f7afb97c3825734a3efaa930
Hubert Guan <hguan@ucsb.edu>
2025-07-02 18:16:21 -0700

    Fix redefinition of MIN

--- deflate.c
+++ deflate.c
@@ -1615,7 +1615,9 @@ local void check_match(deflate_state *s, IPos start, IPos match, int length) {
 #define MAX_STORED 65535
 
 /* Minimum of a and b. */
+#ifndef MIN
 #define MIN(a, b) ((a) > (b) ? (b) : (a))
+#endif // !MIN
 
 /* ===========================================================================
  * Copy without compression as much as possible from the input stream, return
